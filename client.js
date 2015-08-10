87b246412e11fa1487ec3d5b8032632efe704664
Zane Sterling <sterling.zane@gmail.com>
2015-08-10 19:55:38 -0400

    Set up socket poc

--- client.js
+++ client.js
@@ -1,10 +1,14 @@
 document.body.onload = function() {
+	var socketIO = require('socket.io-client');
+
 	var config = require('./client/config');
 	var Renderer = require('./client/Renderer');
 	var GameState = require('./client/GameState');
 
 	var renderer;
 	var gameState;
+
+	var socket = socketIO();
 
 	var init = function() {
 		gameState = new GameState();
--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 "use strict";
 
 var express = require('express');
-var Simulator = require('./server_build/Simulator');
+var http = require('http');
+var socketIo = require('socket.io');
 
 var app = express();
+var appServer = http.Server(app);
+var io = socketIo(appServer);
+
+var Simulator = require('./server_build/Simulator');
 
 app.use('/public/', express.static('public'));
 app.get('/', function(req, res) {
 	res.sendFile('./public/index.html', { root: __dirname });
 });
 
-app.listen(8000);
+io.on('connection', function(socket) {
+	console.log('client connected');
+	socket.on('disconnect', function() {
+		console.log('client disconnected');
+	});
+});
+
+appServer.listen(8000);
 
 Simulator.run(function() {
 });
